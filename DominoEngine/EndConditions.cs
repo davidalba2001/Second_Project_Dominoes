51a5031e55ef7519ae8c107b5e6602db1277904d
davidalba <david2001.004@gmail.com>
2022-06-20 00:27:24 +0200

    Minor improvements to game completion conditions(Algunas mejoras)

--- DominoEngine/EndConditions.cs
+++ DominoEngine/EndConditions.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace DominoEngine
 {
-    public class EndCriteria
+    public static class EndConditions<TValue> where TValue:IEquatable<TValue>
     {
-        
+        public static bool IsLocked(List<Player<TValue>> players)
+        {
+            foreach (var item in players)
+            {
+                if(!item.step) return false;
+            }
+            
+            return true;
+        } 
     }
 }
