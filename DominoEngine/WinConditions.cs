11a75fc070b6e9ebbc4f4e7c7800e9e8c959723d
davidalba <david2001.004@gmail.com>
2022-06-20 00:32:15 +0200

    Implement win conditions if player's chips run out and winner by number of chips //(Implemente las condiciones necesarias para conocer el ganador)

--- DominoEngine/WinConditions.cs
+++ DominoEngine/WinConditions.cs
@@ -5,8 +5,51 @@ using System.Threading.Tasks;
 
 namespace DominoEngine
 {
-    public class WinConditions
+    public class WinConditions<TValue> where TValue:IEquatable<TValue>
     {
-        
+        public static bool PlayAllChips(List<Player<TValue>> players ,out Player<TValue> winner)
+        {
+            foreach (var item in players)
+            {
+                if(item.NumChips == 0)
+                { 
+                    winner = item;
+                    return true;
+                }
+            }
+            winner = null;
+            return false;
+        }
+        //public static bool WinnerScore(List<Player<TValue>> players,out Player<TValue> winner)
+        //{
+        //    foreach (var item in player)
+        //    {
+        //        
+        //    }
+        //}
+        public static bool WinnerForChips(List<Player<TValue>> players,out Player<TValue> winner)
+        {
+            Player<TValue> temp = players[0];
+            int minChips = players[0].NumChips;
+            bool flag = false;
+
+            for(int i = 1;i < players.Count;i++ )
+            {
+                if(minChips > players[i].NumChips)
+                {
+                    temp = players[i];
+                    minChips = players[i].NumChips;
+                    flag = true;
+                }
+            }
+            if(flag)
+            {
+                winner = temp;
+                return true;
+            }
+            winner = null;
+            return false; 
+        }
+
     }
 }
