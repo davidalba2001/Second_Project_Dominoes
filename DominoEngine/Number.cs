848ac1021b2c3cc01afc938fddf136441127d8b6
Jose Carlos <josecarlospendasrodriguez@gmail.com>
2022-06-21 15:08:43 -0400

    Crear la cara de tipo Numerico

---
+++ DominoEngine/Number.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DominoEngine
+{
+    public class Number : IEquatable<Number>
+    {
+        public Number(int number)
+        {
+            this.number = number;
+        }
+
+        int number{get;}
+
+        public bool Equals(Number? other)
+        {
+            if(this.number==other.number) return true;
+            else return false;
+        }
+    }
+}
