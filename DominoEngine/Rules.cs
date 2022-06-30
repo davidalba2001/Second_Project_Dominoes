f1142d21956f5bf0691ea637ff1811c88835fc2f
davidalba <david2001.004@gmail.com>
2022-06-30 14:49:41 +0200

    modificacion de genericidad

--- DominoEngine/Rules.cs
+++ DominoEngine/Rules.cs
@@ -6,57 +6,56 @@ using System.Threading.Tasks;
 
 namespace DominoEngine
 {
-    public class Rules<TValue> where TValue : IEquatable<TValue>
+    public class Rules<T>
     {
 
-        private delegate bool WinCondition(List<Player<TValue>> players, out Player<TValue> player);
-        private delegate bool LockConditions(List<Player<TValue>> players);
+        private delegate bool WinCondition(List<Player<T>> players, out Player<T>? player);
+        private delegate bool LockConditions(List<Player<T>> players);
 
-        public int NumPlayers { get; private set;}
+        public int NumPlayers { get; private set; }
         public int NumChips { get; }
-        public bool PlayIsValid(Chip<TValue> chip, TValue value)
+        public bool PlayIsValid(Chip<T> chip, IValue<T> value)
         {
-            return chip.LinkL.Equals(value) || chip.LinkR.Equals(value);
+            return chip.LinkL.Equals(value) || chip.LinkR.Equals(value)|| value == null;
         }
         public bool IsTurnToPlay(int turn, int playerOrder)
         {
             return turn % NumPlayers == playerOrder;
         }
-        public bool IsFinal(List<Player<TValue>> players)
+        public bool IsFinal(List<Player<T>> players)
         {
-            LockConditions locked = new LockConditions(EndConditions<TValue>.IsLocked);
+            LockConditions locked = new LockConditions(EndConditions<T>.IsLocked);
             return locked(players);
         }
-
-        public bool IsWinner(List<Player<TValue>> players, out Player<TValue> player)
+        public bool IsWinner(List<Player<T>> players, out Player<T>? player)
         {
-            WinCondition playAllChips = new WinCondition(WinConditions<TValue>.PlayAllChips);
-            WinCondition winForChips  = new WinCondition(WinConditions<TValue>.WinnerForChips);
-            if (playAllChips(players,out player)||IsFinal(players))
+            WinCondition playAllChips = new WinCondition(WinConditions<T>.PlayAllChips);
+            WinCondition winForChips = new WinCondition(WinConditions<T>.WinnerForChips);
+            if (playAllChips(players, out player) || IsFinal(players))
             {
-                if(player != null) return true;
+                if (player != null) return true;
                 else
                 {
-                    return winForChips(players,out player);
+                    return winForChips(players, out player);
                 }
             }
             else return false;
         }
-        public List<Chip<Number>> GetFichas()
+        public List<Chip<T>> GenerateChips(int cant, List<IValue<T>> values)
         {
-            List<Chip<Number>> Chips = new List<Chip<Number>>();
-            for(int i = 0; i<10; i++)
+            List<Chip<T>> Chips = new List<Chip<T>>();
+            for (int i = 0; i < cant; i++)
             {
-                for(int j=1; j<10; j++)
+                for (int j = 1; j < cant; j++)
                 {
-                    Chips.Add(new Chip<Number>(new Number(i),new Number(j)));
+                    Chips.Add(new Chip<T>(values[i], values[j]));
                 }
             }
             return Chips;
         }
-        public void SetNumPlayers(int N)
+        public void SetNumPlayers(int n)
         {
-            this.NumPlayers = N;
+            this.NumPlayers = n;
         }
     }
 }
